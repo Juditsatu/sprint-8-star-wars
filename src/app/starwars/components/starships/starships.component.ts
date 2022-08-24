e96a4c02b510a4ce7b8f414f2b023a9c958fd76e
Judit Santos <judit.sant@gmail.com>
2022-08-24 20:14:29 +0200

    added api starships and interface

--- src/app/starwars/components/starships/starships.component.ts
+++ src/app/starwars/components/starships/starships.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StarshipsService } from '../../services/starships.service';
+
+import { Starship } from '../../interfaces/starship.interface';
 
 @Component({
   selector: 'app-starships',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarshipsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private starshipService: StarshipsService) { }
 
   ngOnInit(): void {
+
+    this.getStarships();
+
+  }
+
+  getStarships() {
+    this.starshipService.getStarships()
+      .subscribe({
+        next: (res: Starship) => {
+          console.log(res)
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
   }
 
 }
--- src/app/starwars/services/starships.service.ts
+++ src/app/starwars/services/starships.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Starship } from '../interfaces/starship.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarshipsService {
 
-  constructor() { }
+  private apiUrl: string = 'https://swapi.dev/api/starships/?page=1';
+
+  constructor( private http: HttpClient ) { }
+
+  getStarships(): Observable<Starship> {
+    const url = `${this.apiUrl}`
+    return this.http.get<Starship>(url);
+  }
 }
