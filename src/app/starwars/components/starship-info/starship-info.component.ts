799d5b45d1cf9b408756b894f1afe6fbcc465e7d
Judit Santos <judit.sant@gmail.com>
2022-08-25 18:25:30 +0200

    added images in starship-info

--- src/app/starwars/components/starship-info/starship-info.component.ts
+++ src/app/starwars/components/starship-info/starship-info.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap, tap } from 'rxjs';
 
 import { Result } from '../../interfaces/starship.interface';
 import { StarshipsService } from '../../services/starships.service';
+import { IdPipe } from '../../pipes/id.pipe';
 
 @Component({
   selector: 'app-starship-info',
@@ -17,13 +18,24 @@ export class StarshipInfoComponent implements OnInit {
 
   @Input() starship!: Result;
 
+  urlImg: string = 'https://starwars-visualguide.com/assets/img/starships/';
+  noImage: string = 'https://starwars-visualguide.com/assets/img/big-placeholder.jpg';
+  idImg: string = '';
+
   ngOnInit(): void {
     this.activateRoute.params
       .pipe(
         switchMap(({ id }) => this.starshipService.getStarship(id)),
         tap(console.log)
       )
-      .subscribe(starship => this.starship = starship)
+      .subscribe(starship => {
+        this.starship = starship
+        this.idImg = starship.url.replace(/\D/g, '');
+      });
+  }
+
+  getImage(): string {
+    return `${this.urlImg + this.idImg}.jpg`;
   }
 
 }
--- src/app/starwars/starwars.module.ts
+++ src/app/starwars/starwars.module.ts
@@ -5,7 +5,6 @@ import { StarwarsRoutingModule } from './starwars-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { StarshipsComponent } from './components/starships/starships.component';
 import { StarshipInfoComponent } from './components/starship-info/starship-info.component';
-
 import { IdPipe } from './pipes/id.pipe';
 
 
